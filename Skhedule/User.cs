6536937eb63833988996b7e2a77acdc7dbef48b8
Wiweka Yoga <70740913+wiweka24@users.noreply.github.com>
2021-04-23 02:05:03 +0700

    Add files via upload

---
+++ Skhedule/User.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skhedule
+{
+    class User
+    {
+        //Instance
+        private string usserUname;
+        private string usserPass;
+        private string usserEmail;
+        private string usserID;
+
+        //Properties
+        public string Ussername
+        {
+            get { return usserUname; }
+            set { usserUname = value; }
+        }
+
+        public string Password
+        {
+            get { return usserPass; }
+            set { usserPass = value; }
+        }
+
+        public string Email
+        {
+            get { return usserEmail; }
+            set { usserEmail = value; }
+        }
+
+        public string ID
+        {
+            get { return usserID; }
+            set { usserID = value; }
+        }
+
+        //Cosntructor
+        public User() { }
+        public User(string formUssername, string formPassword)
+        {
+            Ussername = formUssername;
+            Password = formPassword;
+        }
+
+        //Method
+        public void signUp()
+        {
+            //usser add new ussername and password
+        }
+
+        public bool login(string formUssername, string formPassword)
+        {
+            //dummy login
+            if (formUssername == "admin" & formPassword == "admin")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void addAct()
+        {
+            //add new activity (name, deadline, desc, type)
+        }
+
+        public void editAct()
+        {
+            //edit activity detail
+        }
+    }
+}
