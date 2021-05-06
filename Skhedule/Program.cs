443d3dc82670056791cd704a5092cb521bc4656b
Wiweka Yoga <70740913+wiweka24@users.noreply.github.com>
2021-05-07 00:34:57 +0700

    Add files via upload

--- Skhedule/Program.cs
+++ Skhedule/Program.cs
@@ -16,7 +16,7 @@ namespace Skhedule
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new ActForm());
         }
     }
 }
